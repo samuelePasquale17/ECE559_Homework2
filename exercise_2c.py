6da1d3c0f38c626cbadfe0e242c9aef338c681b5
Samuele Pasquale <samuele.pasquale01@gmail.com>
2024-09-09 23:35:26 -0500

    Second exercise done

--- exercise_2c.py
+++ exercise_2c.py
@@ -1,3 +1,7 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
 def MAT_step(matrix):
     """
     Function that given a matrix in input generates a new matrices applying the
@@ -94,9 +98,29 @@ def boolean_func_NN(X, W, b, U, C):
     :param b: first-layer Bias vector
     :param U: second-layer Weight matrix
     :param C: second-layer Bias vector
-    :return: None
+    :return: y
     """
     return MAT_step(MAT_sum(MAT_prod(U, MAT_step(MAT_sum(MAT_prod(W, X), b))), C))
+
+
+def plot_points(points, color):
+    """
+    Function that given a set of points and a color, plots the points using matplotlib
+    :param points: vector of points
+    :param color: vector of either 0's or 1's
+    :return: None
+    """
+
+    # Defining 0 as blue and 1 as red
+    colors = ['blue' if c == 0 else 'red' for c in color]
+
+    # plot
+    plt.scatter(points[:, 0], points[:, 1], c=colors, alpha=0.6)
+    plt.xlabel('X coordinate')
+    plt.ylabel('Y coordinate')
+    plt.title('Scatter Plot of Random Points with Colors')
+    plt.grid(True)
+    plt.show()
 
 
 def main():
@@ -113,10 +137,27 @@ def main():
 
     C = [[-1.5]]
 
+    # Sample 1,000 random points x from a uniform distribution over the square [-2, 2]^2
+    num_points = 1000
+    x_points = []
 
-    # now sample values...
+    # 1,000 points
+    for _ in range(num_points):
+        point = np.random.uniform(-2, 2, 2)
+        x_points.append(point)
+
+    x_points = np.array(x_points)
+
     # compute y by the NN
+    y_res = []
+    for point in x_points:
+        x = [[point[0]],
+             [point[1]]]
+        y = boolean_func_NN(x, W, b, U, C)
+        y_res.append(y[0][0])
+
     # plot
+    plot_points(x_points, y_res)
 
 
 main()
